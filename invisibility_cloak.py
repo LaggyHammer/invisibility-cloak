42267baaece029ed46857a50c910393acd38c99e
laggyhammer <ankch24@gmail.com>
2020-07-12 15:05:29 +0530

    added doc strings & comments

--- invisibility_cloak.py
+++ invisibility_cloak.py
@@ -23,6 +23,14 @@ def get_arguments():
 
 
 def color_mask(hsv, color, upper, lower):
+    """
+    Given a color OR an upper/lower range in the HSV color space, generate a mask for the same.
+    :param hsv: image in HSV color space
+    :param color: color for mask
+    :param upper: upper bound for HSV color mask range
+    :param lower: lower bound for HSV color mask range
+    :return: mask of desired color/range
+    """
     if color is not None:
 
         if color.lower() == 'blue':
@@ -52,6 +60,11 @@ def color_mask(hsv, color, upper, lower):
 
 
 def save_background(video_feed):
+    """
+    Waits for 3 seconds then returns the 60th web-cam feed frame as the background frame
+    :param video_feed: video capture feed to be used
+    :return: frame to be used as background
+    """
     time.sleep(3)
 
     background = 0
@@ -64,6 +77,13 @@ def save_background(video_feed):
 
 
 def create_videowriter(file_name, fps, size):
+    """
+    Creates a video writer object to save the video feed module results
+    :param file_name: file to save the video in
+    :param fps: frames per second for the video
+    :param size: size/resolution of the video feed
+    :return: video writer object to write to
+    """
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(file_name, fourcc, fps, size)
 
@@ -72,34 +92,47 @@ def create_videowriter(file_name, fps, size):
 
 def main(file_name='output.avi', fps=20.0, size=(640, 480), color=None, upper=None, lower=None):
 
+    # Define video writer
     out = create_videowriter(file_name, fps, size)
 
+    # Define video feed
     cap = cv2.VideoCapture(0)
 
+    # Saving background
     background = save_background(video_feed=cap)
 
     count = 0
 
+    # Main loop
     while cap.isOpened():
         ret, img = cap.read()
         if not ret:
             break
         count += 1
+
+        # flip image to show mirrored image
         img = np.flip(img, axis=1)
 
+        # change to HSV color space
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+        # get color mask
         mask1 = color_mask(hsv, color, upper, lower)
 
+        # open & dilate the mask to make mask clearer
         mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
         mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
 
+        # background mask
         mask2 = cv2.bitwise_not(mask1)
 
+        # invisibility cloak
         res1 = cv2.bitwise_and(img, img, mask=mask2)
 
+        # background
         res2 = cv2.bitwise_and(background, background, mask=mask1)
 
+        # adding both results & displaying
         final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
         out.write(final_output)
         cv2.imshow("Invisibility Cloak", final_output)
@@ -108,6 +141,7 @@ def main(file_name='output.avi', fps=20.0, size=(640, 480), color=None, upper=No
         if key == ord('q'):
             break
 
+    # releasing the video capture & closing windows
     cap.release()
     out.release()
     cv2.destroyAllWindows()
